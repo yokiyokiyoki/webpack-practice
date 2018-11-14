041e4bf4e5e5d62f35c524da7c813de15a196a5f
youjie <youjie@hudongpai.com>
2018-11-14 19:58:47 +0800

    新增热更新，虽然webpack-dev-server也可以，但是是reload整个页面

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "none",
@@ -17,6 +18,7 @@ module.exports = {
         test: /\.css$/,
         //顺序是从右到左处理，先css进来然后style动态插入到文档里
         use: ["style-loader", "css-loader"]
+        //style-loader已经实现hmr的接口
       },
       {
         //file-loader接受任何类型文件，并返回处理。url-loader与其类似，只不过它可以接受一个限制值，限制值前可以转成dataUrl
@@ -31,10 +33,13 @@ module.exports = {
     //动态插入文档
     new HtmlWebpackPlugin({
       title: "起步"
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   devtool: "inline-source-map",
   devServer: {
-    contentBase: path.resolve(__dirname, "dist")
+    contentBase: path.resolve(__dirname, "dist"),
+    //热更新开启，跟hmr配合使用
+    hot: true
   }
 };
