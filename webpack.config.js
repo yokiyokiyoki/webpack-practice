bc3f2cd86ff556621e45b62dff4a6199265ab719
youjie <youjie@hudongpai.com>
2018-11-14 15:52:33 +0800

    动态生成html文档

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 //支持无配置，但是有项目也需要复杂的配置
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "none",
   entry: { app: path.resolve(__dirname, "src/index.js") },
@@ -21,6 +22,11 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "起步"
+    })
+  ],
   devServer: {
     contentBase: "./dist"
   }
