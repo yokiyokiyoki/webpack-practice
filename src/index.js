9b079c2f858234687c7e6c22e2bdf3b714c94e67
youjie <youjie@hudongpai.com>
2018-11-14 15:23:48 +0800

    解析图片，如果是在css里面css-loader会自动识别，如果是在html里面那么需要html-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import "./style.css";
+import png from "./img/en.png";
 function component() {
-  var element = document.createElement("div");
+  let element = document.createElement("div");
 
   element.innerHTML = "hello world";
   element.classList.add("hello");
+
+  let img = new Image();
+  img.src = png;
+
+  element.appendChild(img);
+
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         test: /\.css$/,
         //顺序是从右到左处理，先css进来然后style动态插入到文档里
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|gif|svg|jpg)$/,
+        use: ["file-loader"]
       }
     ]
   },
