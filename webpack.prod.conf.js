396908ece39c9223e1f19d1ba357c684ca81c872
youjie <youjie@hudongpai.com>
2018-11-15 22:01:22 +0800

    代码压缩

--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -3,12 +3,16 @@ const path = require("path");
 const common = require("./webpack.base.conf");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const webpack = require("webpack");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const config = webpackMerge(common, {
   mode: "production",
   //单独生成source-map，为bundle添加了一个引用注释
   devtool: "source-map",
   plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
     //每次构建先清理dist
     new CleanWebpackPlugin(["dist"])
   ]
