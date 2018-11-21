ef33fbaf2e58343f5cca8c45eb6eea9153248f9d
youjie <youjie@hudongpai.com>
2018-11-21 18:41:28 +0800

    eslint运行prettier

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import './style.css'
-import png from './img/en.png'
+import "./style.css";
+import png from "./img/en.png";
 
-import Vue from 'vue'
+import Vue from "vue";
 
-function component () {
-  let element = document.createElement('div')
+function component() {
+  let element = document.createElement("div");
 
-  element.innerHTML = 'hello world'
-  element.classList.add('hello')
+  element.innerHTML = "hello world";
+  element.classList.add("hello");
 
-  let img = new Image()
-  img.src = png
+  let img = new Image();
+  img.src = png;
 
-  element.appendChild(img)
+  element.appendChild(img);
 
-  console.log(Vue)
-  ;[1, 2, 3].map(n => n + 1)
+  console.log(Vue);
+  [1, 2, 3].map(n => n + 1);
 
-  return element
+  Promise.resolve(6)
+    .then(d => {
+      return d;
+    })
+    .then(a => {
+      console.log(a);
+    });
+
+  return element;
 }
 
-document.body.appendChild(component())
+document.body.appendChild(component());
